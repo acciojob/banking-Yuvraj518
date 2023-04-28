fe47780819061b7c9e50a2e3d1b95f08ae108f90
Rajan Chaudhary <Yuvraj518>
2023-04-28 18:36:07 +0530

    Third commit

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -16,7 +16,6 @@ public class CurrentAccount extends BankAccount{
             throw new Exception("Insufficient Balance");
         }
         this.tradeLicenseId=tradeLicenseId;
-        validateLicenseId();
     }
 
     public void validateLicenseId() throws Exception {
@@ -24,32 +23,86 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-        if(!tradeLicenseId.equals(tradeLicenseId.toUpperCase())  || !isValidTradeId()){
-            throw new Exception("Valid License can not be generated");
+        int n = tradeLicenseId.length();
+        int temp = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (tradeLicenseId.charAt(i) == tradeLicenseId.charAt(i + 1))
+                break;
+            else {
+                if (tradeLicenseId.charAt(i) != tradeLicenseId.charAt(i + 1))
+                    temp++;
+            }
         }
-//        while(!isValidTradeId()){
-//            char ch[]=tradeLicenseId.toCharArray();
-//        }
+        if (temp == n - 1)
+            return;
+        int freq[] = new int[26];
+        for (int i = 0; i < n; i++)
+            freq[tradeLicenseId.charAt(i) - 'A']++;
+        int max = 0, letter = 0;
+        for (int i = 0; i < 26; i++) {
+            if (max < freq[i]) {
+                max = freq[i];
+                letter = i;
+            }
+        }
+        if (max > (n + 1) / 2)
+            throw new Exception("Valid License can not be generated");
+        char ch[] = new char[n];
+
+        // For Even position filling
+        for (int i = 0; i < n; i += 2) {
+            if (freq[letter] > 0) {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+
+        // For Odd position filling
+        for (int i = 1; i < n; i += 2) {
+            if (freq[letter] > 0) {
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            } else {
+                for (int j = 0; j < 26; j++) {
+                    if (freq[j] > 0) {
+                        letter = j;
+                        break;
+                    }
+                }
+                ch[i] = (char) (letter + 'A');
+                freq[letter]--;
+            }
+        }
+        tradeLicenseId = String.valueOf(ch);
         return;
     }
-    private boolean isValidTradeId(){
-        HashMap<Character,Integer> hm=new HashMap<>();
-        for(int i=0;i<tradeLicenseId.length()-1;i++){
-            char p=tradeLicenseId.charAt(i);
-            hm.put(p,hm.getOrDefault(p,0)+1);
-        }
-        int checkLength;
-        if(tradeLicenseId.length()%2==0){
-            checkLength=((tradeLicenseId.length()+1)/2);
-        }
-        else{
-            checkLength=((tradeLicenseId.length())/2);
-        }
-        int x;
-        if(x: hm.values()){
-            if(x>checkLength){return false;}
-        }
-        return true;
-    }
+//    private boolean isValidTradeId(){
+//        HashMap<Character,Integer> hm=new HashMap<>();
+//        for(int i=0;i<tradeLicenseId.length()-1;i++){
+//            char p=tradeLicenseId.charAt(i);
+//            hm.put(p,hm.getOrDefault(p,0)+1);
+//        }
+//        int checkLength;
+//        if(tradeLicenseId.length()%2==0){
+//            checkLength=((tradeLicenseId.length()+1)/2);
+//        }
+//        else{
+//            checkLength=((tradeLicenseId.length())/2);
+//        }
+//        int x;
+//        if(x: hm.values()){
+//            if(x>checkLength){return false;}
+//        }
+//        return true;
+//    }
 
 }
